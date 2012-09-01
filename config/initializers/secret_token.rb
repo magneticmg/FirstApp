87f3dd457b3acc1c8b7978d5a6ad1612ae57fef2
magneticmg <magneticmg@ubuntu.(none)>
2012-09-01 12:13:25 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '81c143169903ff3c2e5936caf3b060ac5c74891c658a10a6b068933b6b207de4f214592baf466064c513868dd7f4dd3c4f0e46b50a3bec5fa72576ae1e57d9ca'
